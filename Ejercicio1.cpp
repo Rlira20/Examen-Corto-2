27881cca11450b17feb8639e3d2c700e42a2278e
Rlira20 <00124424@uca.edu.sv>
2024-06-02 09:35:58 -0600

    Primeros dos ejercicios del corto

---
+++ Ejercicio1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Se declaran variales.
+    float a1, a2, base, area;
+    cout << "-----------CALCULO DE AREA DEL TRAPECIO-----------\nRecuerda que el trapecio que quiere calcular tiene un lado mas pequenio que el otro. \n";
+    //Se solicita al usuario que ingrese los valores del trapecio para calcular su área y se guardan en las variables específicas.
+    cout << "Ingrese el valor de la primera altura: ";
+    cin >> a1;
+    cout << "Ingrese el valor de la segunda altura: ";
+    cin >> a2;
+    cout << "Ingrese el valor de la Base: ";
+    cin >> base;
+    
+    //Se utiliza una condición if debido que el trapecio de la imagen no tiene alturas iguales. 
+    //Este if ayuda a diferenciar las alturas para saber que no tienen que ser iguales.
+    if(a1 != a2){
+        //Se hace la operación del área, según la forma que se brinda.
+        area = (a1+a2/2)*base;   
+        //Se le da el valor del área al usuario.
+        cout << "El valor del area del trapecio es de: "<< area<< ". \n";
+
+    } else{
+        //En dado caso que sean iguales, mostrará el siguiente mensaje. 
+        cout << "Sus alturas son de la misma altura, no es el trapecio esperado:(... ";
+    }
+    
+    return 0;
+}
---
+++ Ejercicio2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //se declaran variables.
+    float rango;
+    int decision;
+    cout << "-----------ESCALA TERMICA PARA PACIENTES CON FIEBRE.----------- \n";
+    
+    //Se utiliza un do-while para preguntarle al usuario si ingresará otro paciente para saber su escala.
+    do{
+        //se le solicita al usuario que ingrese el rango de temperatura en que se encuentra el paciente.
+        cout << "Ingrese el rango de temperatura en que se encuentra el paciente: ";
+        cin >> rango;
+        
+        //se utiliza la condicion 'Else-if' debido que es el más adecuado para manejar rango de valores.
+        //en cada if o else-if se hace una comparacián de valores para determinar en qué escala se encuentra el paciente
+        if(rango >= 37.4 , rango <=37.9 ){
+            //Por ejemplo, si los datos ingresados guardados en la variable 'rango' estan en la comparación del if, mostrará lo siguiente.
+            cout << "Su paciente se encuentra en la escala: Febricula. \n";
+        } else if(rango >= 38.0, rango <= 38.9){
+            cout << "Su paciente se encuentra en la escala: Fiebre moderada. \n";
+        } else if(rango >= 39.0, rango <= 39.9){
+            cout << "Su paciente se encuentra en la escala: Fiebre alta. \n";
+        } else if(rango >= 40.0, rango <= 41.5){
+            cout << "Su paciente se encuentra en la escala: Fiebre muy alta. \n";
+        } else if(rango > 41.5){
+            cout << "Su paciente se encuentra en la escala: Hiperporexia. \n";
+        } else if(rango < 37.4){
+            cout << "Su paciente se encuentra en la escala: Hipotermia. \n";
+        } else {
+            cout << "Sus valores son incorrectos *_*";
+        }
+        
+        //Se pregunta al usuario si ingresará más datos, si elige 1, volverá a ingresar, si elije 2, entonces saldrá del bucle.
+        cout << "¿Desea ingresar a otro paciente?(escoja un numero) \n1.Si \n2.no \n";
+        cin >> decision;
+        
+    }while(decision == 1);
+    
+    return 0;
+}
